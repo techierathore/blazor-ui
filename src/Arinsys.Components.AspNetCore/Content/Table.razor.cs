e85cc6bd8bbae467167e0adf73e7041051800f25
abhisheksiddhu <abhisheksiddhu@outlook.in>
2020-09-26 19:11:49 +0530

    make data accessor async in table

--- src/Arinsys.Components.AspNetCore/Content/Table.razor.cs
+++ src/Arinsys.Components.AspNetCore/Content/Table.razor.cs
@@ -3,6 +3,8 @@
 using System;
 using System.Collections.Generic;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
 
 namespace Arinsys.Components.AspNetCore.Content
 {
@@ -19,16 +21,15 @@ namespace Arinsys.Components.AspNetCore.Content
 
     public partial class Table<TEntity, TColumnDefinition, TTableDataFilters> : BaseComponent
         where TColumnDefinition : ColumnDefinition<TEntity>
-        where TTableDataFilters : TableDataFilters<TEntity>
+        where TTableDataFilters : TableDataFilters<TEntity>, new()
     {
+        internal readonly BehaviorSubject<object> filtersObservable = new BehaviorSubject<object>(null);
+
         [Parameter]
         public IEnumerable<TColumnDefinition> ColumnDefinitions { get; set; }
 
         [Parameter]
-        public virtual IObservable<IEnumerable<TEntity>> DataObservable { get; set; }
-
-        [Parameter]
-        public virtual Func<TTableDataFilters, IEnumerable<TEntity>> DataAccessor { get; set; }
+        public virtual Func<TTableDataFilters, Task<IEnumerable<TEntity>>> DataAccessor { get; set; }
 
         [Parameter]
         public RenderFragment<TEntity> Body { get; set; }
@@ -39,14 +40,18 @@ namespace Arinsys.Components.AspNetCore.Content
         [Parameter]
         public RenderFragment LoadingContent { get; set; }
 
+        public IObservable<TTableDataFilters> FiltersObservable => filtersObservable.Select(_ => TableDataFilters);
+        public TTableDataFilters TableDataFilters { get; private set; } = new TTableDataFilters();
+        public void TableDataFiltersUpdated() => filtersObservable.OnNext(null);
+
+
         protected IEnumerable<TEntity> Data { get; private set; }
         protected TTableDataFilters Filters { get; set; }
 
-        protected override void OnInitialized()
+        protected override async Task OnInitializedAsync()
         {
-            base.OnInitialized();
-            if (DataAccessor != null) Data = DataAccessor(Filters);
-            if (DataObservable != null) ChangeStateOn(DataObservable?.Do(data => Data = data));
+            await base.OnInitializedAsync();
+            if (DataAccessor != null) Data = await DataAccessor(Filters);
         }
     }
 }
